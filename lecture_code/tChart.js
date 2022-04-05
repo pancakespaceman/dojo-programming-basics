684ec8c6728136ba755e2d05566aced516d0093b
Shawn Converse <shawn.a.converse@outlook.com>
2022-04-05 13:11:12 -0700

    w1d2 commit

---
+++ lecture_code/tChart.js
@@ -0,0 +1,28 @@
+var num1 = 20;
+var num2 = 5;
+if (num1 < num2) {
+  num2 = num2 * num1;
+} else {
+  num1 = num1 / num2;
+  if (num1 < num2) {
+    num1 = num1 * 2;
+  } else if (num1 == num2) {
+    num2 = num1 * num2;
+  } else {
+    num2 = num2 * 2;
+  }
+}
+
+console.log(num1);
+console.log(num2);
+console.log("Hello from a separate js file!");
+console.log("I need to rewatch Star Wars again (not the sequals)")
+console.log("This is a different change")
+
+
+
+// Logical operators
+
+AND operator
+OR operator
+NOT operator
