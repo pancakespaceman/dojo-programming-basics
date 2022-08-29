9146d4e571e26a985e7205a5eca7feba7f6875e0
Alfredo08 <alfredosv08@gmail.com>
2022-08-29 16:32:51 -0500

    Variables and Data types

---
+++ lectures/week1/Day 1/dataTypes.js
@@ -0,0 +1,32 @@
+
+// Variable with a 'number'
+var age = 25;
+/*
+    Variable
+    holding
+    a
+    'string'
+*/
+let firstName = "Alex";
+
+// Variable with a 'boolean'
+var isMarried = false;
+
+// As a standar we use camelCase
+// Variable with a 'null'
+
+let lastName = null;
+
+//lastName = "Miller";
+
+// Variable with a 'undefined'
+let stack;
+let program = undefined;
+
+console.log( age );
+console.log( firstName, lastName );
+console.log( "Are you married? " + isMarried );
+console.log( isMarried );
+console.log( stack, program );
+
+
