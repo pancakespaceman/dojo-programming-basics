ec72e1026c1873bc7ed2510c373368e6b8be7446
Alfredo08 <alfredosv08@gmail.com>
2022-09-02 12:02:45 -0500

    Arrays

---
+++ lectures/week1/Day 5/NeuralNetworks.js
@@ -0,0 +1,16 @@
+/*
+    Smith, Robinson, and Jones
+    Prompt: 
+    On a train, Smith, Robinson, and Jones are the fireman, brakeman, and the engineer, 
+    but not necessarily respectively. Also aboard the train are three businessmen who have 
+    the same names: a Mr. Smith, a Mr. Robinson, and a Mr. Jones. Using the clues below, 
+    can you determine the identity of the Engineer?
+
+    Clues: 
+    1. Mr. Robinson lives in Detroit.
+    2. The brakeman lives exactly halfway between Chicago and Detroit
+    3. Mr. Jones earns exactly $20,000 per year.
+    4. The brakeman’s nearest neighbor, one of the passengers, earns exactly three times as much as the brakeman.
+    5. Smith beats the fireman in billiards.
+    6. The passenger whose name is the same as the brakeman’s lives in Chicago.
+*/
