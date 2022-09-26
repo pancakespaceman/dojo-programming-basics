c80abf2f634190381dd12ac76f229a682a2d7a19
Shawn Allen Converse <68838748+shawnaconverse@users.noreply.github.com>
2022-09-26 13:06:52 -0700

    w1d1 commit

---
+++ lectures/week1/day1/myFile.js
@@ -0,0 +1,53 @@
+// console.log("Hello There!")
+
+// console.log("Here is another console log")
+
+// This is a comment
+// Comments are ignored by the computer
+// They allow us to write notes in our code
+
+// ======== VARIABLES ==========
+
+// How to make a variable
+// var keyword followed by the name (label) of the variable (box)
+// I put data in the variable using the assignment operator, AKA the equal sign
+// Read this line of code Right side of = first, then left
+var firstName = "Winter"
+
+firstName = "Uchenna"
+
+firstName = "Jeffrey"
+
+console.log("Hello, " + firstName);
+
+// if you don't see quotes (single or double) its a variable
+// if you see quotes, its a String
+
+// Naming Conventions
+// Variable names should describe their contents
+
+// is this a good variable name?
+var aaaaaaa = "Pizza"
+console.log(aaaaaaa);
+
+var myFavoriteFood = "Pizza"
+console.log(myFavoriteFood)
+
+// We follow conventions as programmers so that things are easier to read and understand
+// Variable names in JS use camelCase, or 
+// the first word is lowercase, and every next word is uppercase (the first leter)
+// NO SPACES
+var myFavoriteFood = "pasta"
+
+// THE copmuter does not care what the name is
+
+var isThisAValidVariable = "Yes or no?"
+console.log(isThisAValidVariable)
+
+var my_favorite_food_is_a_special_type_of_pasta_dish_know_as_kraft_mac_n_cheese = "yummy"
+console.log(my_favorite_food_is_a_special_type_of_pasta_dish_know_as_kraft_mac_n_cheese)
+
+
+
+var dfjasdfhasldkfhjalksdjfalksdhfjlaskdcfjhaosirjgaslkdvjasipdgjhaslrkjvdflzxkjgvasdlkfgjarsilgdfzjvlxkcghjbsazdiogjqawsdflknzvboijfdaklgajszxfiocvbjfklghjqreifnzxkldbvjnszlrikfnglkadfjncbvoinmadlskrghjaeirlfndvbnaeoringalskcxhvbnaordifgnaorisd = "hahahahahaa"
+console.log(dfjasdfhasldkfhjalksdjfalksdhfjlaskdcfjhaosirjgaslkdvjasipdgjhaslrkjvdflzxkjgvasdlkfgjarsilgdfzjvlxkcghjbsazdiogjqawsdflknzvboijfdaklgajszxfiocvbjfklghjqreifnzxkldbvjnszlrikfnglkadfjncbvoinmadlskrghjaeirlfndvbnaeoringalskcxhvbnaordifgnaorisd)
