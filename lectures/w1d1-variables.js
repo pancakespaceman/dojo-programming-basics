820730e691c98cd61da5ce4366e92624133611ea
Shawn Converse <shawn.a.converse@outlook.com>
2024-07-01 13:59:54 -0700

    w1d1

---
+++ lectures/w1d1-variables.js
@@ -0,0 +1,87 @@
+// Variables
+
+// comment - text we write in our code for us the developer
+// comments are not executed as code
+
+// vs code has a hotkey for commenting lines of code
+// windows: ctrl + /
+// mac: cmd + /
+
+// to make a variable in JS, we need to use the var keyword
+// sytax: var variableName = data
+
+var myVariable = "data";
+
+// the = is called the assignment operator
+// console.log("this is a string inside of the console.log()");
+// console.log(myVariable);
+
+// ctrl + s / cmd + s
+
+var greenLightNumOfSeconds = 20;
+
+// console.log(greenLightNumOfSeconds);
+
+
+// Variable Naming Conventions
+
+// In JS, we use camelCase (lowerCamelCase) for variables and function names
+// UpperCamelCase is used for class names (PascalCase)
+
+// variables should describe what data is contained in the variable
+
+// var thing1 = "Shawn"
+var flahsdofuahncvhnadosunadsvalodhfalsedhflachvlaudhfloasdnvlahcvlahdouanseofacsnvouadhfoahesfoacnmvoandmogadnmgoaucsnvoasedmashonacolsvnaosdfnaosidvnalsnvaosnvoascnvoasdnvoasdnv = "Shawn"
+var thing1 = "Shawn"
+var firstName = "Shawn"
+
+
+console.log(flahsdofuahncvhnadosunadsvalodhfalsedhflachvlaudhfloasdnvlahcvlahdouanseofacsnvouadhfoahesfoacnmvoandmogadnmgoaucsnvoasedmashonacolsvnaosdfnaosidvnalsnvaosnvoascnvoasdnvoasdnv)
+
+
+// Strings
+// a string of characters (AKA text)
+// syntax: the text surrounded by a pair of single quotes (apostrophes) or double quotes
+'My String'
+"Another String"
+
+"This apple is your's"
+
+
+// Numbers
+// JS combines whole numbers (aka integers) and floats (aka decimals) into a single data type
+var x = 5;
+var y = 10;
+
+// + - / *
+
+console.log(5 + 10)
+console.log(x + y)
+
+
+// Boolean
+// true or false
+// binary 0 and 1
+
+console.log(true)
+console.log(false)
+
+var isOldEnoughToRide = false
+
+
+
+// Undefined
+// what is the value of a variable if you never assign it?
+var newVar;
+
+console.log(newVar)
+
+
+// Null
+// statement that something is empty
+// represents nothing
+// placeholder
+
+var anotherVar = null;
+
+console.log(anotherVar)
