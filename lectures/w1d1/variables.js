99f37150a267fb4fcb9335b7ecd446573eb366bf
Shawn Converse <shawn.a.converse@outlook.com>
2024-08-26 14:05:54 -0700

    w1d1

---
+++ lectures/w1d1/variables.js
@@ -0,0 +1,85 @@
+// This is a comment
+// Windows: ctrl + /
+// Mac: cmd + /
+// console.log("this is a string")
+
+
+
+// Variables
+// boxes that hold data
+// contents can change (variable root word is vary)
+// generally want to give our variables names that describe what data they hold
+// Syntax: var varName = value
+
+// the single equal sign is called the assignment operator
+// take the stuff on the right, and put it in the box on the left
+var firstName = "Shawn"
+
+// to access the value in a variable, we simple write the name of a variable
+// console.log(firstName)
+
+// to change a variable value, we DO NOT need the var keyword
+firstName = "Cory"
+
+// console.log(firstName)
+
+// the computer doesn't care what the variable name is!
+var afaldsfhasdhfaclxvhoadsnfadsnvlacxhfgalsdhgnalcskvhnaldfhalsdhfalsdcvnmzldhfgaslnzcxlvfhadslfanlscvalxcvfalsdfnladsfnalsdvncxvnadsonasdnvlacxvladsnfladshflasdhfladshflsdhlacxhvladshf = "hahahaha"
+
+// console.log(afaldsfhasdhfaclxvhoadsnfadsnvlacxhfgalsdhgnalcskvhnaldfhalsdhfalsdcvnmzldhfgaslnzcxlvfhadslfanlscvalxcvfalsdfnladsfnalsdvncxvnadsonasdnvlacxvladsnfladshflasdhfladshflsdhlacxhvladshf)
+
+// Who reads code? us, humans
+
+// Naming Conventions
+// in JavaScript, we use camelCase (lowerCamelCase)
+
+var lastName = "Converse"
+var dateOfBirth = "yes"
+
+// Data Types
+// Number 
+
+var num1 = 5.81
+var num2 = 10.35
+var num3 = num1 + num2 // 5 + 10 = 15
+console.log(num3)
+
+// Boolean
+// true or false
+// this stems from binary
+var isSunny = true
+console.log("Check if its sunny outside:", isSunny)
+
+
+// String
+// a singular piece of data
+// just a collection of characters (a bunch of words together)
+// syntax: string must be surrounded by either single quotes (apostrophys) or double quotes
+var myString1 = 'Here is a string with single quotes'
+var myString2 = "Here is a string with double quotes"
+
+// console.log(myString1)
+// console.log(myString2)
+
+
+// var thing = "something"
+
+// console.log(thing)
+
+// concatenate strings is combinging them together
+var myString3 = myString1 + myString2
+console.log(myString3)
+
+
+// Data types that represent the concept of no data
+// Undefined and Null
+
+// undefined means we made a box but never said if there's anything inside
+var undefinedVariable
+
+console.log(undefinedVariable)
+
+
+var nullVariable = null
+console.log(nullVariable)
+
